696c451c4d2d245f945944b27f2287714f27b673
Christopher Kenrick <christopher.kenrick@gmail.com>
2016-03-02 12:27:18 -0500

    final practice app without style

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: 'Javascript: The Good Parts'},
-		{ title: 'Harry Potter'},
-		{ title: 'The Dark Tower'},
-		{ title: 'Eloquent'}
+		{ title: 'Javascript: The Good Parts', pages: 275},
+		{ title: 'Harry Potter', pages: 543},
+		{ title: 'The Dark Tower', pages: 1101},
+		{ title: 'Eloquent', pages: 347}
 	]
 }
